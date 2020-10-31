38aa92ce0a33e622ed40ca9e7e8e7b38fa2762ac
XFrank888 <60580054+XFrank888@users.noreply.github.com>
2020-10-31 14:41:11 -0400

    Date change

--- Date.cpp
+++ Date.cpp
@@ -1,3 +1,17 @@
 #include "Date.h"
+#include <time.h>
+#include <iostream>
 
-int Date::getDays() { return (clock() / CLOCKS_PER_SEC) / dayUnit; }
+using namespace std;
+
+
+int Date::getDays() 
+{ 
+	clock_t start, end;
+	double x = 1.5;
+
+	start = clock();
+	end = clock();
+	cout << "time generate" << (double)(end - start) / CLOCKS_PER_SEC << endl;
+	return (clock() / CLOCKS_PER_SEC) / dayUnit; 
+}
